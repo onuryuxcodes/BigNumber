b09e58f00c34ffaaf15e22e817abcd31fd3ef157
Onur Yuksel <yksl.onr@gmail.com>
2022-07-07 00:20:51 +0300

    base class activity navigation

--- app/src/main/java/io/hidro/bignumber/view/BaseActivity.kt
+++ app/src/main/java/io/hidro/bignumber/view/BaseActivity.kt
@@ -1,9 +1,8 @@
 package io.hidro.bignumber.view
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity() {
-
-    fun goToActivity() {
-
-    }
+    fun goToActivity(intent: Intent) = startActivity(intent)
 }
