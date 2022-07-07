1591f891fb2ff12d63be2d8cd2bbd8f37b947146
Onur Yuksel <yksl.onr@gmail.com>
2022-07-07 13:55:50 +0300

    add button and click navigation

--- app/src/main/java/io/hidro/bignumber/view/MainActivity.kt
+++ app/src/main/java/io/hidro/bignumber/view/MainActivity.kt
@@ -2,12 +2,19 @@ package io.hidro.bignumber.view
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import io.hidro.bignumber.R
+import io.hidro.bignumber.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        goToActivity(Intent(this, BigNumberGameActivity::class.java))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.play.setOnClickListener {
+            goToActivity(Intent(this, BigNumberGameActivity::class.java))
+        }
     }
 }
