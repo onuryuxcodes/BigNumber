db341afaf1aa2b0e6fd1d6dde605bd9677d81d75
Onur Yuksel <yksl.onr@gmail.com>
2022-07-13 02:21:41 +0300

    add navigation constants

--- app/src/main/java/io/hidro/bignumber/util/Constants.kt
+++ app/src/main/java/io/hidro/bignumber/util/Constants.kt
@@ -3,5 +3,9 @@ package io.hidro.bignumber.util
 class Constants {
     companion object{
         const val allowedTimeInMsForEachStep = 5000
+        const val additionalTimeInMsForEachLevel = 3000
+        const val SCORE_KEY = "SCORE"
+        const val HIGH_SCORE_KEY = "HIGH_SCORE"
+
     }
 }
