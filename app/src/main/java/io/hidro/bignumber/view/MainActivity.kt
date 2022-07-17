eb8b6f9901cb5500b8cb74322cd4489c792a07a0
Onur Yuksel <yksl.onr@gmail.com>
2022-07-17 19:35:11 +0300

    update google play services json

--- app/src/main/java/io/hidro/bignumber/view/MainActivity.kt
+++ app/src/main/java/io/hidro/bignumber/view/MainActivity.kt
@@ -3,7 +3,6 @@ package io.hidro.bignumber.view
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
