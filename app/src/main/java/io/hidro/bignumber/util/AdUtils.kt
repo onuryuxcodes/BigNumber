ce0f4386aa5ac9db548dd1ba404e87a48354ed82
Onur Yuksel <yksl.onr@gmail.com>
2022-07-18 22:48:51 +0300

    remove native ad for now

--- app/src/main/java/io/hidro/bignumber/util/AdUtils.kt
+++ app/src/main/java/io/hidro/bignumber/util/AdUtils.kt
@@ -1,13 +1,9 @@
 package io.hidro.bignumber.util
 
 import com.google.android.gms.ads.nativead.NativeAd
-import io.hidro.bignumber.databinding.NativeAdViewBinding
 
 class AdUtils {
     companion object {
-        fun showAdOnView(ad: NativeAd, viewBinding:NativeAdViewBinding) {
-            viewBinding.adTitle.text = ad.headline
-            viewBinding.adSubtitle.text = ad.advertiser
-        }
+
     }
 }
