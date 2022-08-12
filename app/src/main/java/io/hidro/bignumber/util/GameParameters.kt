4683026a3c5fdc40d515d2da8e3c7a50a1e40941
Onur Yuksel <yksl.onr@gmail.com>
2022-08-12 12:15:37 +0200

    update countOfGamePlayToShowRatePopup

--- app/src/main/java/io/hidro/bignumber/util/GameParameters.kt
+++ app/src/main/java/io/hidro/bignumber/util/GameParameters.kt
@@ -4,7 +4,7 @@ class GameParameters {
     companion object {
         const val allowedTimeInMsForEachStep = 5000
         const val additionalTimeInMsForEachLevel = 0
-        const val countOfGamePlayToShowRatePopup = 4
+        const val countOfGamePlayToShowRatePopup = 20
         const val successfulGamePlayMinScore = 100
         const val stepsEachLevel = 20.0
     }
